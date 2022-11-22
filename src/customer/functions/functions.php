b82f6ba0591f6d1c9491b42331ecb0f1f799bb3b
praful <prafulnc01@gmail.com>
2022-11-22 19:21:40 +0530

    new

--- src/customer/functions/functions.php
+++ src/customer/functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "tagency");
+$con = mysqli_connect("tagency.c7vb75mjfxjx.ap-south-1.rds.amazonaws.com", "admin", "admin123", "tagency");
 
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
--- src/customer/includes/db.php
+++ src/customer/includes/db.php
@@ -1,6 +1,10 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "tagency");
+
+$con = mysqli_connect("tagency.c7vb75mjfxjx.ap-south-1.rds.amazonaws.com","admin","admin124","tagency");
+
 if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        echo "Failed to connect to MySQL: ".mysqli_connect_error();
 }
+
 ?>
+
