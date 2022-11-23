50c053c916235b03bfe3e3cf8900c13cef7d4a50
Praful Choudhary <108887894+prafulnc14@users.noreply.github.com>
2022-11-23 11:21:25 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -5,9 +5,11 @@ PATH=$PATH:/usr/local/bin; export PATH
 REGION=ap-south-1
 REPOSITORY_NAME=travel-agency
 CLUSTER=Demo_cluster
-FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
-NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
-SERVICE_NAME=${NAME}
+#FAMILY=`sed -n 's/.*"family": "\(.*\)",/\1/p' taskdef.json`
+#NAME=`sed -n 's/.*"name": "\(.*\)",/\1/p' taskdef.json`
+#SERVICE_NAME=${NAME}
+FAMILY= travel-agency
+NAME= travel-name
 env
 aws configure list
 echo $HOME
