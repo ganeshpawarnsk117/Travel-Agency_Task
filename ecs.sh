f0730fa9cfe38360b70f3e5e7d5038aacf7582c5
Praful Choudhary <108887894+prafulnc14@users.noreply.github.com>
2022-11-23 13:01:56 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -7,7 +7,7 @@ IMAGE_NAME=234111192759.dkr.ecr.ap-south-1.amazonaws.com/travel-agency:latest
 
 #deploy () {
 #0install add jq https://apps.0install.net/utils/jq.xml
-#export PATH=/var/lib/jenkins/bin:$PATH
+export PATH=/var/lib/jenkins/bin:$PATH
 #sleep 20
 whoami
 echo "Deploying to Service"
