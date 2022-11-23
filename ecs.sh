670ca9e344177af2d3713b6fd89ab62374e0d415
Praful Choudhary <108887894+prafulnc14@users.noreply.github.com>
2022-11-23 12:53:06 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -3,11 +3,12 @@ set -x
 export AWS_DEFAULT_REGION=ap-south-1
 SERVICE_NAME=travel-agency
 ECS_CLUSTER=Demo_cluster
+IMAGE_NAME=234111192759.dkr.ecr.ap-south-1.amazonaws.com/travel-agency:latest
 
 #deploy () {
 
 echo "Deploying to Service"
-ecs-deploy -c $ECS_CLUSTER -n $SERVICE_NAME -t 300 --max-definitions 10
+ecs-deploy -c $ECS_CLUSTER -n $SERVICE_NAME -i $IMAGE_NAME -t 300 --max-definitions 10
 
 #}
 #deploy
