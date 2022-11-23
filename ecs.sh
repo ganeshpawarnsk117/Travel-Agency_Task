37af0a176a720448afe0d460aa822c4d5f244b57
Praful Choudhary <108887894+prafulnc14@users.noreply.github.com>
2022-11-23 12:58:31 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -6,7 +6,8 @@ ECS_CLUSTER=Demo_cluster
 IMAGE_NAME=234111192759.dkr.ecr.ap-south-1.amazonaws.com/travel-agency:latest
 
 #deploy () {
-
+0install add jq https://apps.0install.net/utils/jq.xml
+sleep 20
 whoami
 echo "Deploying to Service"
 ecs-deploy -c $ECS_CLUSTER -n $SERVICE_NAME -i $IMAGE_NAME -t 300 --max-definitions 10
