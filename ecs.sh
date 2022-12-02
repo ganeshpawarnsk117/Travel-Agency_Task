ccc87c69916c41480a1513f122f6f1cb2810ecc6
Praful Choudhary <108887894+prafulnc14@users.noreply.github.com>
2022-12-02 12:37:45 +0530

    Update ecs.sh

--- ecs.sh
+++ ecs.sh
@@ -2,7 +2,7 @@
 set -x
 export AWS_DEFAULT_REGION=ap-south-1
 SERVICE_NAME=travel-agency
-ECS_CLUSTER=Demo_cluster
+ECS_CLUSTER=Travel_cluster
 IMAGE_NAME=234111192759.dkr.ecr.ap-south-1.amazonaws.com/travel-agency:latest
 
 #deploy () {
